b1eba9ae37e58bb2609d0ac66a252b07a107fad8
C'est une pomme <dmgordienko@gmail.com>
2022-12-24 13:24:35 +0400

    Initial Commit

---
+++ Barber/BarberApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarberApp.swift
+//  Barber
+//
+//  Created by Дмитрий Гордиенко on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BarberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
